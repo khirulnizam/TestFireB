5a699a08620bac715dfcddd1b7acfa42f475faeb
khirulnizam <kerul76>
2018-07-24 11:56:59 +0800

    class TrainingDetails

--- app/src/main/java/my/edu/kuis/fstm/testfireb/TrainingDetails.java
+++ app/src/main/java/my/edu/kuis/fstm/testfireb/TrainingDetails.java
@@ -2,5 +2,34 @@ package my.edu.kuis.fstm.testfireb;
 
 public class TrainingDetails {
     private String trainingID, trainingname, contact, website;
-    
-}
+
+    public TrainingDetails(){
+        //constructor
+    }
+
+    public void setTrainingID(String trainingID){
+        this.trainingID=trainingID;
+    }
+    public String getTrainingID(){
+        return trainingID;
+    }
+    public void setTrainingname(String trainingname){
+        this.trainingname=trainingname;
+    }
+    public String getTrainingname(){
+        return trainingname;
+    }
+    public void setContact(String contact){
+        this.contact=contact;
+    }
+    public String getContact(){
+        return contact;
+    }
+    public void setWebsite(String website){
+        this.website=website;
+    }
+    public String getWebsite(){
+        return website;
+    }
+
+}//end class TrainingDetails
